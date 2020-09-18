8ce8b42c88b36bc2f4914d0ba8c5866b87867fb8
kennytm <kennytm@gmail.com>
2020-09-18 18:11:58 +0800

    packages: removed, to be replaced by the versions published on NPM

--- packages/grafana-ui/src/components/MatchersUI/fieldMatchersUI.ts
+++
@@ -1,9 +0,0 @@
-import { Registry } from '@grafana/data';
-import { fieldNameMatcherItem } from './FieldNameMatcherEditor';
-import { FieldMatcherUIRegistryItem } from './types';
-import { fieldNameByRegexMatcherItem } from './FieldNameByRegexMatcherEditor';
-import { fieldTypeMatcherItem } from './FieldTypeMatcherEditor';
-
-export const fieldMatchersUI = new Registry<FieldMatcherUIRegistryItem<any>>(() => {
-  return [fieldNameMatcherItem, fieldNameByRegexMatcherItem, fieldTypeMatcherItem];
-});
